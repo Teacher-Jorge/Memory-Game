03ff59b01289faa9c66154d9e8c27b89ef2f291b
Teacher-Jorge <79639664+Teacher-Jorge@users.noreply.github.com>
2022-07-29 14:37:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,58 @@
 const cards = document.querySelectorAll('.memory-card')
 
 let hasFlippedCard = false
+let lockBoard = false
 let firstCard, secondCard
 
 function flipCard() {
-   this.classList.toggle('flip')
+    if (lockBoard) return
+    if (this === firstCard) return
+   this.classList.add('flip')
 
 if (!hasFlippedCard) {
     hasFlippedCard = true
     firstCard = this
-} else {
-    hasFlippedCard = false
+    return
+}
     secondCard = this
 
-if (firstCard.dataset.framework === secondCard.dataset.framework) {
+    checkForMatch()
+ }
+
+
+function checkForMatch() {
+let isMatch = firstCard.dataset.framework === secondCard.dataset.framework
+
+isMatch ? disableCards() : unflipCards()
+}
+
+function disableCards() {
     firstCard.removeEventListener('click', flipCard)
     secondCard.removeEventListener('click', flipCard)
-} else {
 
+    resetBoard()
+}
+
+function unflipCards() {
+    lockBoard = true
     setTimeout(() => {
         firstCard.classList.remove('flip')
         secondCard.classList.remove('flip')
-    }, 1000)
 
-  
-  }
- }
+        resetBoard()
+    }, 1000)
 }
 
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false,false]
+    [firstCard, secondCard] = [null, null]
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(math.random() * 20)
+        card.getElementsByClassName.order = randomPos
+    })
+})()
 
 cards.forEach(card => card.addEventListener('click', flipCard))
