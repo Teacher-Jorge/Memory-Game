7d3436dc1c556bb9542c5e62bfbb61e2a9b6a244
Teacher-Jorge <79639664+Teacher-Jorge@users.noreply.github.com>
2022-07-28 21:15:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const cards = document.querySelectorAll('.memory-card')
+
+let hasFlippedCard = false
+let firstCard, secondCard
+
+function flipCard() {
+   this.classList.toggle('flip')
+
+if (!hasFlippedCard) {
+    hasFlippedCard = true
+    firstCard = this
+} else {
+    hasFlippedCard = false
+    secondCard = this
+
+if (firstCard.dataset.framework === secondCard.dataset.framework) {
+    firstCard.removeEventListener('click', flipCard)
+    secondCard.removeEventListener('click', flipCard)
+} else {
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip')
+        secondCard.classList.remove('flip')
+    }, 1500)
+
+  
+  }
+ }
+}
+
+
+cards.forEach(card => card.addEventListener('click', flipCard))
